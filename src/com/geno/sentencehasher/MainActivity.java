ed4d24c02bd0fbe24c26728e9c0c26594b2cf3d6
Geno1024 <754097987@qq.com>
2015-01-27 14:14:27 +0800

    Many many things

--- src/com/geno/sentencehasher/MainActivity.java
+++ src/com/geno/sentencehasher/MainActivity.java
@@ -7,6 +7,7 @@ import android.widget.*;
 import android.content.*;
 import android.text.*;
 import android.view.View.*;
+import java.security.*;
 
 public class MainActivity extends Activity
 {
@@ -29,14 +30,26 @@ public class MainActivity extends Activity
 		text=(EditText)findViewById(R.id.text);
 		back=(TableLayout)findViewById(R.id.back);
 		calc=(Button)findViewById(R.id.calc);
-		if(sp.getBoolean("dark",false))dark();
+		if (sp.getBoolean("dark", false))dark();
 		calc.setOnClickListener
 		(new OnClickListener()
 			{
 				@Override
 				public void onClick(View p1)
 				{
-					md5in.setText(text.getText().toString().hashCode());
+					md5in.setText(hash(text.getText().toString(),"MD5").toUpperCase());
+					sha1in.setText(hash(text.getText().toString(),"SHA1").toUpperCase());
+					
+				}
+			}
+		);
+		md5in.setOnClickListener
+		(new OnClickListener()
+			{
+				@Override
+				public void onClick(View p1)
+				{
+					clip(md5in);
 				}
 			}
 		);
@@ -48,4 +61,44 @@ public class MainActivity extends Activity
 		sha1.setTextColor(0xFF808080);
 		back.setBackgroundColor(0xFF808080);
 	}
+	
+	void clip(EditText t)
+	{
+		android.content.ClipboardManager cm = (android.content.ClipboardManager)getSystemService(Context.CLIPBOARD_SERVICE);
+		cm.setText(t.getText().toString());
+	}
+
+	public static String hash(String str,String algorithm)  
+	{  
+		MessageDigest md5 = null;  
+		try  
+		{  
+			md5 = MessageDigest.getInstance(algorithm); 
+		}catch(Exception e)  
+		{  
+			e.printStackTrace();  
+			return "";  
+		}  
+
+		char[] charArray = str.toCharArray();  
+		byte[] byteArray = new byte[charArray.length];  
+
+		for(int i = 0; i < charArray.length; i++)  
+		{  
+			byteArray[i] = (byte)charArray[i];  
+		}  
+		byte[] md5Bytes = md5.digest(byteArray);  
+
+		StringBuffer hexValue = new StringBuffer();  
+		for( int i = 0; i < md5Bytes.length; i++)  
+		{  
+			int val = ((int)md5Bytes[i])&0xff;  
+			if(val < 16)  
+			{  
+				hexValue.append("0");  
+			}  
+			hexValue.append(Integer.toHexString(val));  
+		}  
+		return hexValue.toString();  
+	} 
 }
