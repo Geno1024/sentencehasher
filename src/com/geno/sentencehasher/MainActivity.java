3c375ed163e6a5d4f085d254188d638920fec743
Geno1024 <754097987@qq.com>
2015-01-30 13:13:47 +0800

    Finished v1

--- src/com/geno/sentencehasher/MainActivity.java
+++ src/com/geno/sentencehasher/MainActivity.java
@@ -11,35 +11,37 @@ import java.security.*;
 
 public class MainActivity extends Activity
 {
-	public SharedPreferences sp;
-	public SharedPreferences.Editor e;
-	public TextView md5,sha1,sha256;
-	public EditText md5in,sha1in,sha256in,text;
-	public TableLayout back;
+	public TextView md5,sha1,sha256,sha384,sha512,avail;
+	public EditText md5in,sha1in,sha256in,sha384in,sha512in,text;
 	public Button calc;
 	@Override
 	public void onCreate(Bundle savedInstanceState)
 	{
 		super.onCreate(savedInstanceState);
-		sp=getSharedPreferences("Default",MODE_PRIVATE);
-		setContentView(R.layout.light);
+		setContentView(R.layout.main);
+
+		avail=(TextView)findViewById(R.id.avail);
+		avail.setText(getString(R.string.intro)+"\n"+getString(R.string.avail)+Security.getAlgorithms("MessageDigest"));
+
 		md5=(TextView)findViewById(R.id.md5);
 		md5in=(EditText)findViewById(R.id.md5in);
-		
+
 		sha1=(TextView)findViewById(R.id.sha1);
 		sha1in=(EditText)findViewById(R.id.sha1in);
 
 		sha256=(TextView)findViewById(R.id.sha256);
 		sha256in=(EditText)findViewById(R.id.sha256in);
-		
+
+		sha384=(TextView)findViewById(R.id.sha384);
+		sha384in=(EditText)findViewById(R.id.sha384in);
+
+		sha512=(TextView)findViewById(R.id.sha512);
+		sha512in=(EditText)findViewById(R.id.sha512in);
+
 		text=(EditText)findViewById(R.id.text);
-		
-		back=(TableLayout)findViewById(R.id.back);
-		
+
 		calc=(Button)findViewById(R.id.calc);
-		
-		if (sp.getBoolean("dark", false))dark();
-		
+
 		calc.setOnClickListener
 		(new OnClickListener()
 			{
@@ -49,6 +51,8 @@ public class MainActivity extends Activity
 					md5in.setText(hash(text.getText().toString(),"MD5").toUpperCase());
 					sha1in.setText(hash(text.getText().toString(),"SHA1").toUpperCase());
 					sha256in.setText(hash(text.getText().toString(),"SHA256").toUpperCase());
+					sha384in.setText(hash(text.getText().toString(),"SHA384").toUpperCase());
+					sha512in.setText(hash(text.getText().toString(),"SHA512").toUpperCase());
 				}
 			}
 		);
@@ -82,52 +86,67 @@ public class MainActivity extends Activity
 				}
 			}
 		);
+		sha384in.setOnClickListener
+		(new OnClickListener()
+			{
+				@Override
+				public void onClick(View p1)
+				{
+					clip(sha384in);
+				}
+			}
+		);
+		sha512in.setOnClickListener
+		(new OnClickListener()
+			{
+				@Override
+				public void onClick(View p1)
+				{
+					clip(sha512in);
+				}
+			}
+		);
 	}
-	
-	void dark()
-	{
-		md5.setTextColor(0xFFFFFFFF);
-		sha1.setTextColor(0xFFFFFFFFF);
-		back.setBackgroundColor(0xFF808080);
-	}
-	
+
 	void clip(EditText t)
 	{
 		android.content.ClipboardManager cm = (android.content.ClipboardManager)getSystemService(Context.CLIPBOARD_SERVICE);
 		cm.setText(t.getText().toString());
+		Toast.makeText(this,getString(R.string.copied),Toast.LENGTH_SHORT).show();
 	}
 
-	public static String hash(String str,String algorithm)  
-	{  
-		MessageDigest hashcode = null;  
-		try  
+	String hash(String str,String algorithm)
+	{
+		MessageDigest hashcode = null;
+		try
+		{
+			hashcode = MessageDigest.getInstance(algorithm);
+		}
+		catch(Exception e)
 		{  
-			hashcode = MessageDigest.getInstance(algorithm); 
-		}catch(Exception e)  
-		{  
-			e.printStackTrace();  
-			return "";  
+			e.printStackTrace();
 		}  
 
-		char[] charArray = str.toCharArray();  
-		byte[] byteArray = new byte[charArray.length];  
+		char[] charArray = str.toCharArray();
+		byte[] byteArray = new byte[charArray.length];
 
-		for(int i = 0; i < charArray.length; i++)  
-		{  
-			byteArray[i] = (byte)charArray[i];  
-		}  
-		byte[] hashBytes = hashcode.digest(byteArray);  
+		for(int i = 0; i < charArray.length; i++)
+		{
+			byteArray[i] = (byte)charArray[i];
+		}
+		byte[] hashBytes = hashcode.digest(byteArray);
 
-		StringBuffer hexValue = new StringBuffer();  
-		for( int i = 0; i < hashBytes.length; i++)  
-		{  
-			int val = ((int)hashBytes[i])&0xff;  
-			if(val < 16)  
-			{  
-				hexValue.append("0");  
-			}  
-			hexValue.append(Integer.toHexString(val));  
-		}  
-		return hexValue.toString();  
-	} 
+		StringBuffer hexValue = new StringBuffer();
+		for( int i = 0; i < hashBytes.length; i++)
+		{
+			int val = ((int)hashBytes[i])&0xff;
+			if(val < 16)
+			{
+				hexValue.append("0");
+			}
+			hexValue.append(Integer.toHexString(val));
+		}
+		return hexValue.toString();
+	}
 }
+
