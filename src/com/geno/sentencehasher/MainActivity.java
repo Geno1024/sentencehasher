5da41da3d98d90d89586ffc1b1047064d2ad432a
Geno1024 <754097987@qq.com>
2015-02-03 13:28:22 +0800

    what

--- src/com/geno/sentencehasher/MainActivity.java
+++ src/com/geno/sentencehasher/MainActivity.java
@@ -42,9 +42,13 @@ public class MainActivity extends Activity
 
 		intext=new TextWatcher() 
 		{
-			@Override public void onTextChanged(CharSequence s,int start,int before,int count){}
+			@Override public void onTextChanged(CharSequence s,int start,int before,int count)
+			{
+			}
 
-			@Override public void beforeTextChanged(CharSequence s,int start,int count,int after){}
+			@Override public void beforeTextChanged(CharSequence s,int start,int count,int after)
+			{
+			}
 
 			@Override
 			public void afterTextChanged(Editable s)
